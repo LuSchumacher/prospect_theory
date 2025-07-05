de1b03c3285e9b26d28637cd6be0b9c0c0b717bd
LuSchumacher <lu.schumacher@bluewin.ch>
2025-07-05 12:55:26 +0200

    update stan model

--- src/stan/model_fitting.R
+++ src/stan/model_fitting.R
@@ -6,74 +6,106 @@ library(bayesplot)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 df <- read_csv('../../data/pilot_2_data_prepped_excluded.csv')
-df <- df %>% 
-  filter(id == 1)
+# df <- df %>% 
+#   filter(id == 1)
 
 N <- length(unique(df$id))
 `T` <- nrow(df)
 stan_data = list(
   `T`        = `T`,
-  # N          = N,
-  # subject_id = df$id,
+  N          = N,
+  subject_id = df$id,
   condition  = ifelse(df$condition == "old", 0, 1),
   outcome_a  = as.matrix(df[, c("outcome_a1", "outcome_a2", "outcome_a3")]),
   outcome_b  = as.matrix(df[, c("outcome_b1", "outcome_b2", "outcome_b3")]),
   choice    = df$resp
 )
 
-init_fun <- function(chains = 4, N) {
-  inits <- vector("list", chains)
-  for (i in 1:chains) {
-    inits[[i]] <- list(
-      mu_lambda_0 = 2 + runif(1, -0.5, 0.5),
-      mu_lambda_1 = 0 + runif(1, -0.2, 0.2),
-      mu_alpha_0  = 1 + runif(1, -0.5, 0.5),
-      mu_alpha_1  = 0 + runif(1, -0.2, 0.2),
-      mu_tau_0    = 2 + runif(1, -0.5, 0.5),
-      mu_tau_1    = 0 + runif(1, -0.2, 0.2),
-      
-      sigma_lambda_0 = runif(1, 0.2, 0.5),
-      sigma_lambda_1 = runif(1, 0.2, 0.5),
-      sigma_alpha_0  = runif(1, 0.2, 0.5),
-      sigma_alpha_1  = runif(1, 0.2, 0.5),
-      sigma_tau_0    = runif(1, 0.2, 0.5),
-      sigma_tau_1    = runif(1, 0.2, 0.5),
-      
-      lambda_0 = rnorm(N, 2, 0.2),
-      lambda_1 = rnorm(N, 0, 0.1),
-      alpha_0  = rnorm(N, 1, 0.1),
-      alpha_1  = rnorm(N, 0, 0.1),
-      tau_0    = rnorm(N, 2, 0.2),
-      tau_1    = rnorm(N, 0, 0.1)
-    )
-  }
-  return(inits)
-}
-
+# init_fun <- function(chains = 4, N) {
+#   inits <- vector("list", chains)
+#   for (i in 1:chains) {
+#     inits[[i]] <- list(
+#       mu_lambda_0 = 2 + runif(1, -0.5, 0.5),
+#       mu_lambda_1 = 0 + runif(1, -0.2, 0.2),
+#       mu_alpha_0  = 1 + runif(1, -0.5, 0.5),
+#       mu_alpha_1  = 0 + runif(1, -0.2, 0.2),
+#       mu_tau_0    = 2 + runif(1, -0.5, 0.5),
+#       mu_tau_1    = 0 + runif(1, -0.2, 0.2),
+#       
+#       sigma_lambda_0 = runif(1, 0.2, 0.5),
+#       sigma_lambda_1 = runif(1, 0.2, 0.5),
+#       sigma_alpha_0  = runif(1, 0.2, 0.5),
+#       sigma_alpha_1  = runif(1, 0.2, 0.5),
+#       sigma_tau_0    = runif(1, 0.2, 0.5),
+#       sigma_tau_1    = runif(1, 0.2, 0.5),
+#       
+#       lambda_0 = rnorm(N, 2, 0.2),
+#       lambda_1 = rnorm(N, 0, 0.1),
+#       alpha_0  = rnorm(N, 1, 0.1),
+#       alpha_1  = rnorm(N, 0, 0.1),
+#       tau_0    = rnorm(N, 2, 0.2),
+#       tau_1    = rnorm(N, 0, 0.1)
+#     )
+#   }
+#   return(inits)
+# }
+# 
 init_fun <- function(chains = 4) {
   inits <- vector("list", chains)
   for (i in 1:chains) {
     inits[[i]] <- list(
-      lambda_0 = runif(1, 0.5, 2.5),
-      lambda_1 = runif(1, 0.5, 2.5),
-      alpha_0  = runif(1, 0.3, 0.9),
-      alpha_1  = runif(1, 0.3, 0.9),
-      tau_0    = runif(1, 0.2, 1.5),
-      tau_1    = runif(1, 0.2, 1.5)
+      mu_lambda_0    = rnorm(1, log(2), 0.5),
+      mu_lambda_1    = rnorm(1, log(2), 0.5),
+      mu_alpha_0     = rnorm(1, 1.0, 1.75),
+      mu_alpha_1     = rnorm(1, 1.0, 1.75),
+      mu_tau_0       = rnorm(1, 0.5, 0.5),
+      mu_tau_1       = rnorm(1, 0.5, 0.5),
+      sigma_lambda_0 = rnorm(1, 0, 2),
+      sigma_lambda_1 = rnorm(1, 0, 2),
+      sigma_alpha_0  = rnorm(1, 0, 2),
+      sigma_alpha_1  = rnorm(1, 0, 2),
+      sigma_tau_0    = rnorm(1, 0, 2),
+      sigma_tau_1    = rnorm(1, 0, 2),
+      z_lambda_0     = rnorm(N, 0, 1),
+      z_lambda_1     = rnorm(N, 0, 1),
+      z_alpha_0      = rnorm(N, 0, 1),
+      z_alpha_1      = rnorm(N, 0, 1),
+      z_tau_0        = rnorm(N, 0, 1),
+      z_tau_1        = rnorm(N, 0, 1)
     )
   }
   return(inits)
 }
 
 
+# init_fun <- function(chains = 4) {
+#   inits <- vector("list", chains)
+#   for (i in 1:chains) {
+#     inits[[i]] <- list(
+#       lambda_0_raw = rnorm(1, log(2), 0.5),
+#       lambda_1_raw = rnorm(1, log(2), 0.5),
+#       alpha_0_raw  = rnorm(1, 1.0, 1.75),
+#       alpha_1_raw  = rnorm(1, 1.0, 1.75),
+#       tau_0_raw    = rnorm(1, 0.5, 0.5),
+#       tau_1_raw    = rnorm(1, 0.5, 0.5)
+#     )
+#   }
+#   return(inits)
+# }
+
+
 # PARAM_NAMES <- c(
-#   "lambda_mean_0", "alpha_mean_0", "tau_mean_0",
-#   "lambda_mean_1", "alpha_mean_1", "tau_mean_1",
-#   "delta_lambda", "delta_alpha", "delta_tau"
+#   "lambda_0", "alpha_0", "tau_0",
+#   "lambda_1", "alpha_1", "tau_1"
 # )
+# 
+PARAM_NAMES <- c(
+  "lambda_0_out", "alpha_0_out", "tau_0_out",
+  "lambda_1_out", "alpha_1_out", "tau_1_out"
+)
 
 pt_model <- cmdstan_model(
-  'pt_single.stan',
+  'pt_single_new.stan',
   cpp_options = list(stan_threads = T)
 )
 
@@ -83,16 +115,48 @@ fit_pt_model <- pt_model$sample(
   max_treedepth = 15,
   adapt_delta = 0.95,
   refresh = 50,
-  iter_sampling = 1000,
-  iter_warmup = 1000,
+  iter_sampling = 2000,
+  iter_warmup = 2000,
   chains = 4,
   parallel_chains = 4,
   threads_per_chain = 2,
   save_warmup = TRUE
 )
 
-fit_pt_model$summary()
-mcmc_trace(fit_pt_model$draws(inc_warmup = TRUE), n_warmup = 200, pars=PARAM_NAMES)
+fit_pt_model$summary(variables = PARAM_NAMES)
+mcmc_trace(
+  fit_pt_model$draws(inc_warmup = TRUE),
+  n_warmup = 2000,
+  pars=PARAM_NAMES
+)
 
+y_rep <- fit_pt_model$draws("y_rep", format = "matrix")
+y_obs <- df$choice
 
+NUM_PP_DRAWS <- 500
+idx <- sample(1:8000, NUM_PP_DRAWS)
+
+pred_data <- y_rep %>% 
+  as_tibble() %>% 
+  mutate(draw = 1:8000) %>% 
+  filter(draw %in% idx) %>% 
+  pivot_longer(-draw, names_to = "trial", values_to = "resp") %>% 
+  mutate(
+    trial = str_extract(trial, "(?<=\\[)\\d+(?=\\])"),
+    condition = rep(df$condition, times=NUM_PP_DRAWS),
+    ev_diff = rep(df$ev_diff, times=NUM_PP_DRAWS)
+  )
+
+pred_summary <- pred_data %>% 
+  group_by(condition, ev_diff) %>% 
+  summarise(mean_resp = mean(resp))
+
+emp_summary <- df %>% 
+  group_by(condition, ev_diff) %>% 
+  summarise(mean_resp = mean(resp))
+
+pred_summary %>% 
+  ggplot(aes(x = ev_diff, y = mean_resp, color = condition)) +
+  geom_point() +
+  geom_point(data = emp_summary, color="black")
 
--- src/stan/pt_single.stan
+++ src/stan/pt_single.stan
@@ -1,5 +1,5 @@
 functions {
-  real pt_utility(vector x, real alpha, real lambda) {
+  real get_pt_utility(vector x, real alpha, real lambda) {
     real u = 0;
     int K = num_elements(x);
     for (k in 1:K) {
@@ -7,9 +7,16 @@ functions {
       if (val >= 0)
         u += pow(val, alpha);
       else
-        u += -lambda * pow(abs(val), alpha);  // fix for negative values
+        u += -lambda * pow(abs(val), alpha);
     }
     return u / K;
+  }
+  real inverse_utility(real u, real alpha, real lambda) {
+    if (u >= 0) {
+      return pow(u, 1 / alpha);
+    } else {
+      return -pow(abs(u) / lambda, 1 / alpha);
+    }
   }
 }
 
@@ -22,30 +29,51 @@ data {
 }
 
 parameters {
-  real<lower=0> lambda_0;
-  real<lower=0> lambda_1;
-  real<lower=0, upper=1> alpha_0;
-  real<lower=0, upper=1> alpha_1;
-  real<lower=0> tau_0;
-  real<lower=0> tau_1;
+  real lambda_0_raw;
+  real lambda_1_raw;
+  real alpha_0_raw;
+  real alpha_1_raw;
+  real tau_0_raw;
+  real tau_1_raw;
+}
+
+transformed parameters {
+  real<lower=0>          lambda_0 = exp(lambda_0_raw);
+  real<lower=0>          lambda_1 = exp(lambda_1_raw);
+  real<lower=0, upper=1> alpha_0  = inv_logit(alpha_0_raw);
+  real<lower=0, upper=1> alpha_1  = inv_logit(alpha_1_raw);
+  real<lower=0>          tau_0    = exp(tau_0_raw);
+  real<lower=0>          tau_1    = exp(tau_1_raw);
 }
 
 model {
-  lambda_0 ~ lognormal(log(1.5), 0.5);
-  lambda_1 ~ lognormal(log(1.5), 0.5);
-  alpha_0  ~ beta(2, 1);
-  alpha_1  ~ beta(2, 1);
-  tau_0    ~ gamma(1.5, 0.5);
-  tau_1    ~ gamma(1.5, 0.5);
+  lambda_0_raw ~ normal(log(2), 0.5);
+  lambda_1_raw ~ normal(log(2), 0.5);
+  alpha_0_raw  ~ normal(1.0, 1.75); 
+  alpha_1_raw  ~ normal(1.0, 1.75);
+  tau_0_raw    ~ normal(0.5, 0.5);
+  tau_1_raw    ~ normal(0.5, 0.5);
 
   for (t in 1:T) {
     real lambda = condition[t] == 0 ? lambda_0 : lambda_1;
     real alpha  = condition[t] == 0 ? alpha_0  : alpha_1;
     real tau    = condition[t] == 0 ? tau_0    : tau_1;
 
-    real u_a = pt_utility(to_vector(outcome_a[t]), alpha, lambda);
-    real u_b = pt_utility(to_vector(outcome_b[t]), alpha, lambda);
+    real u_a_raw = get_pt_utility(to_vector(outcome_a[t]), alpha, lambda);
+    real u_b_raw = get_pt_utility(to_vector(outcome_b[t]), alpha, lambda);
+    // real u_a = inverse_utility(u_a_raw, alpha, lambda);
+    // real u_b = inverse_utility(u_b_raw, alpha, lambda);
 
-    choice[t] ~ bernoulli_logit(tau * (u_b - u_a));
+    choice[t] ~ bernoulli_logit(tau * (u_b_raw - u_a_raw));
   }
 }
+
+generated quantities {
+  real lambda_0_out = exp(lambda_0_raw);
+  real lambda_1_out = exp(lambda_1_raw);
+  real alpha_0_out = inv_logit(alpha_0_raw);
+  real alpha_1_out = inv_logit(alpha_1_raw);
+  real tau_0_out = exp(tau_0_raw);
+  real tau_1_out = exp(tau_1_raw);
+}
+
