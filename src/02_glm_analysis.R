ddc3e77b90f534e7337db63530db4c4fb748e4b1
LuSchumacher <lu.schumacher@bluewin.ch>
2026-01-08 16:58:51 +0100

    update repo

--- src/02_glm_analysis.R
+++ src/02_glm_analysis.R
@@ -11,24 +11,29 @@ COLOR_PALETTE <- c('#27374D', '#B70404')
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-df <- read_csv('../data/study_data_prepared.csv')
+df <- read_csv('../data/study_data_prepared.csv') %>% 
+  mutate(gamble_type = as_factor(gamble_type))
+
+contrasts(df$gamble_type) <- rbind(-0.5, 0.5)
 
 model_formula <- resp ~ ev_diff * gamble_type + (ev_diff * gamble_type | id)
-
 model_priors <- prior(normal(0, 1.0), class = b)
+
 glm_fit <- brm(
   formula = model_formula,
   data = df,
-  family =
+  family = bernoulli(),
   prior = model_priors,
-  iter = 4000,
+  iter = 8000,
   cores = 4,
   chains = 4,
+  control = list(adapt_delta = 0.85),
   sample_prior = "yes",
   file = "../fits/glm_fit" 
 )
 
 conditional_effects(glm_fit)
+
 bf <- 1 / hypothesis(glm_fit, "gamble_typeunconfounded = 0")$hypothesis["Evid.Ratio"]
 bf <- hypothesis(glm_fit, "gamble_typeunconfounded < 0")$hypothesis["Evid.Ratio"]
 bf <- 1 / hypothesis(glm_fit, "ev_diff = 0")$hypothesis["Evid.Ratio"]
